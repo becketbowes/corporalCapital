dcd65959dde35e8f5db953c20333eefb1853f392
becket <becket.bowes@gmail.com>
2022-02-25 00:04:57 -0500

    functioning objects, functioning inquirer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,19 @@
 const Engineer = require('../lib/Engineer');
 
 test('creates an engineer object', () => {
-    let fellah = new Engineer('Paulie Girl', 'Engineer', 'shit@poop.scat', '458-663-7183', 'random');
+    let fellah = new Engineer('random', 'github');
+    fellah.person = 'Paulie Girl'; 
+    fellah.position = 'Engineer';
+    fellah.email = 'shit@poop.scat';
+    fellah.phone = '458-663-7183';
+    
 
-    expect(fellah.personName).toBe('Paulie Girl');
+
+
+    expect(fellah.person).toBe('Paulie Girl');
     expect(fellah.position).toBe('Engineer');
     expect(fellah.id).toEqual(expect.any(Number));
     expect(fellah.phone).toBe('458-663-7183');
     expect(fellah.email).toBe('shit@poop.scat');
+    expect(fellah.github).toBe('github');
 });
